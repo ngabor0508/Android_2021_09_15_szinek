6d7c33c6a2c18b2b2b5f74f1f7b6e1b412816073
Nagy Gabor <ngabor0508@gmail.com>
2021-12-06 23:36:57 +0100

    Projekt létrehozva.

---
+++ app/src/main/java/com/example/nagy_gabor_2021_09_15_szinek/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nagy_gabor_2021_09_15_szinek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
