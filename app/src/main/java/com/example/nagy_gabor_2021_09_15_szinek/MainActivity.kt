5895386a226a28e9c995c8b83cb87621f21e3bde
Nagy Gabor <ngabor0508@gmail.com>
2021-12-13 17:18:59 +0100

    Id-k beállítva.

--- app/src/main/java/com/example/nagy_gabor_2021_09_15_szinek/MainActivity.kt
+++ app/src/main/java/com/example/nagy_gabor_2021_09_15_szinek/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.nagy_gabor_2021_09_15_szinek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var layout : RelativeLayout
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    fun init(){
+        layout = findViewById(R.id.layout)
+        textView = findViewById(R.id.text_view)
     }
 }
